784186f9d9beed11ade2d5215da3327e7d7f918d
Avni Jain <113444922+AVNIJ17@users.noreply.github.com>
2024-09-23 06:18:56 +0000

    datatypes file added

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hi Avni") // we are using nodejs,not browser
+
+
+// code readabbility is important semi colon is not mandate in js
+console.log(3+1)
+console.log("Avni");
+
+
+//Datatypes -----
+/*
+
+
+number => 2 to power 53
+bigint
+string=>""
+boolean => true/false
+null=> standalone value
+undefined => means value is not assigned yet 
+symbol => unique values 
+
+object
+
+*/ 
+
+//console.log(typeof value) typeof gives whch datatype it is
+
+console.log(typeof undefined) //undefined
+
+console.log(typeof null) // object
