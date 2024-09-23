d6bf5a9fd642cde4f8179a0f87bc31a2bba1152a
Avni Jain <113444922+AVNIJ17@users.noreply.github.com>
2024-09-23 08:33:52 +0000

    operators update in conversionOperators file

--- 01_basics/conversionOperators.js
+++ 01_basics/conversionOperators.js
@@ -22,3 +22,38 @@ let booleanIsLoggedIn =Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn); //false 
 
 //1 =>true 0=> false "" =>false "string" => true
+
+
+//-----------------Operations------------ +-*%/
+
+let str1="Hi"
+let str2 ="Avni "
+
+let str3=str1+str2 
+console.log(str3);
+
+
+console.log("1"+2); //12
+console.log("1"+"2");//12
+console.log(1+"2");//12
+console.log(1+"2"); //12
+console.log(1+2);//3
+console.log("1"+2 + 2); //122
+console.log(1+2+"2");//32
+
+
+console.log(+true); //1
+console.log(+""); //0
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
