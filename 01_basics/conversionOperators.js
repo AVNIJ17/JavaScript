e7b2bd75025cc8731334358301f8d6f6944a77ed
Avni Jain <113444922+AVNIJ17@users.noreply.github.com>
2024-09-23 06:29:06 +0000

    coneversionOperations files added

---
+++ 01_basics/conversionOperators.js
@@ -0,0 +1,24 @@
+let score ="44"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNum =Number(score)
+
+console.log(typeof valueInNum);
+console.log(valueInNum); // sometime it gives vlue sometimes it gives NaN
+
+
+//"44" => 44
+// "44abc" => NaN
+// true => 1 False => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn); //false 
+
+//1 =>true 0=> false "" =>false "string" => true
